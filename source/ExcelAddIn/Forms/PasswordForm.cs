ea86836c181955609b09017ed4a6ef46fea12b14
Javier Treviño <javier.trevino@oracle.com>
2012-05-03 16:02:46 -0500

    - Added PasswordForm to the Forms folder of the project.

---
+++ source/ExcelAddIn/Forms/PasswordForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MySQL.ExcelAddIn
+{
+  public partial class PasswordForm : Form
+  {
+    public string PasswordText
+    {
+      set { txtPassword.Text = value; }
+      get { return txtPassword.Text; }
+    }
+
+    public string HostIdentifier
+    {
+      set { lblConnectionValue.Text = value; }
+    }
+
+    public string UserName
+    {
+      set { lblUserValue.Text = value; }
+    }
+
+    public PasswordForm()
+    {
+      InitializeComponent();
+    }
+  }
+}
