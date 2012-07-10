12b03b6cc601fc7017b6a29dcfc68721a5b40b33
Roberto E. Garcia <robertoe.garcia@oracle.com>
2012-07-10 18:05:53 -0500

    Fixed local/remote connection list based on the host and ssh server (for ssh connections). Added support for null host values.

--- source/Panels/WelcomePanel.cs
+++ source/Panels/WelcomePanel.cs
@@ -13,6 +13,8 @@ namespace MySQL.ForExcel
 {
   public partial class WelcomePanel : AutoStyleableBasePanel
   {
+    private static string[] localValues = new string[] { string.Empty, "127.0.0.1", "localhost", "local" };
+
     public WelcomePanel()
     {
       InitializeComponent();
@@ -38,8 +40,21 @@ namespace MySQL.ForExcel
 
     private void AddConnectionToList(MySqlWorkbenchConnection conn)
     {
-      int nodeIdx = (conn.Host == "127.0.0.1" || conn.Host.ToLowerInvariant() == "localhost" ? 0 : 1);
-      string s = String.Format("{0}|{1}", conn.Name, String.Format("User: {0}, IP: {1}", conn.UserName, conn.Host)); ;
+      int nodeIdx = 1;
+      string hostName = (conn.Host ?? string.Empty).Trim();
+      if (conn.DriverType == MySqlWorkbenchConnectionType.Ssh)
+      {
+        string[] sshConnection = conn.HostIdentifier.Split('@');
+        string dbHost = sshConnection[1].Split(':')[0].Trim();
+        string sshHost = sshConnection[2].Split(':')[0].Trim();
+        if (localValues.Contains(sshHost.ToLowerInvariant()) && localValues.Contains(dbHost.ToLowerInvariant()))
+          nodeIdx = 0;
+        hostName = dbHost + " (ssh)";
+      }
+      else if (localValues.Contains(hostName.ToLowerInvariant()))
+        nodeIdx = 0;
+
+      string s = String.Format("{0}|{1}", conn.Name, String.Format("User: {0}, Host: {1}", conn.UserName, hostName));
       TreeNode node = connectionList.AddNode(connectionList.Nodes[nodeIdx], s);
       node.ImageIndex = 0;
       node.Tag = conn;
