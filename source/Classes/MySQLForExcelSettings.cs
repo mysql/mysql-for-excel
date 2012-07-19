21d9df2be288c20e107fd571f05c7dba0c508931
Reggie Burnett <reggie.burnett@oracle.com>
2012-07-18 19:23:15 -0500

    now uses a custom settings provider so our settings file is in a sane place

---
+++ source/Classes/MySQLForExcelSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySQL.ForExcel.Properties;
+using MySQL.Utility;
+
+namespace MySQL.ForExcel
+{
+  public class MySQLForExcelSettings : CustomSettingsProvider
+  {
+    public override string ApplicationName
+    {
+      get { return Resources.AppName; }
+      set { } 
+    }
+
+    public override string SettingsPath
+    {
+      get { return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Oracle\MySQL For Excel\settings.config"; }
+    }
+  }
+}
